878b56bfc181f4c1bc46685e5c04000fa07471bc
amit <amit.dahiya.j@gmail.com>
2024-12-12 05:07:46 +0000

    added dynamic web project

---
+++ WelcomeServlet/src/main/java/com/java/servlet/Login.java
@@ -0,0 +1,36 @@
+package com.java.servlet;
+
+import jakarta.servlet.http.HttpServlet;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String email = request.getParameter("txtEmail");
+		
+		String password = request.getParameter("txtPassword");
+		
+		System.out.println("Email: " + email);
+		System.out.println("Password: " + password);
+	}
+
+}
---
+++ WelcomeServlet/src/main/java/com/java/servlet/Welcome.java
@@ -0,0 +1,33 @@
+package com.java.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+/**
+ * Servlet implementation class Welcome
+ */
+public class Welcome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Welcome() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("Welcome to my Servlet...");
+	}
+
+}
